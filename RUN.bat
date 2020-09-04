4e27328aa622ead0298e15f2ef6a89598167be2e
Gokul Manohar <gokulmanohar7@gmail.com>
2020-09-04 19:19:13 +0530

    Update RUN.bat

--- RUN.bat
+++ RUN.bat
@@ -1,6 +1,6 @@
 @echo off
 
-"%~dp0\_internal\Python\Python38\python.exe" "%~dp0\script.py"
+"C:\Users\gokul\AppData\Local\Programs\Python\Python38\python.exe" "%~dp0\script.py"
 
 echo Opening Webbrowser . . . 
 echo.
